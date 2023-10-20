f31938aa79212e02332ecc99d2f68f589e10a441
theofor1 <theo.fs2000@gmail.com>
2023-10-20 15:34:15 +0200

    Creating Project

---
+++ PathFindingProject/PathFindingProject.cpp
@@ -0,0 +1,12 @@
+﻿// PathFindingProject.cpp : définit le point d'entrée de l'application.
+//
+
+#include "PathFindingProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
